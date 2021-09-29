3063b458f4d98915b9582203bb20addeedda81cd
Vinicius Costa <viniciustvcosta@outlook.com>
2021-09-29 17:40:29 -0300

    Commit Urna

---
+++ Urna Download/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Bigodê de la France',
+                partido: 'PDS',
+                fotos: [
+                    {url:'38111.jpeg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Troncas Monkas',
+                partido: 'PDT',
+                fotos: [
+                    {url:'77222.jpeg', legenda: 'Vereador'}    
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Bigordo de Taipas',
+                partido: 'PDS',
+                vice: 'Special cat',
+                fotos: [
+                    {url:'99.jpeg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Gordo do Tatuapé',
+                partido: 'PG',
+                vice: 'Demi Glace',
+                fotos: [
+                    {url:'84.jpeg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
